6733b7efaf91dcac6d62f33ade75ec3dd4449226
TariqMehmood1004 <johnbrrighte7427206@gmail.com>
2024-08-15 14:56:24 +0500

    akdjn

--- file-sharing-backend/server.js
+++ file-sharing-backend/server.js
@@ -7,6 +7,8 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const app = express();
 const port = 5000;
 
